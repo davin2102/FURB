dff2923735d64a17c737948d29db2519c5f55912
davin2102 <165655509+davin2102@users.noreply.github.com>
2025-06-18 22:01:30 +0700

    Update vite.config.js

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allows external access
+    host: '0.0.0.0',
+    port: 3000,      
   },
-})
+  build: {
+    outDir: 'dist',  
+    emptyOutDir: true, 
+  },
+  preview: {
+    port: 3000,      
+  }
+});
